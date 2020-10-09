014b11456d26cba6b28819f16ff7e7d1f13286aa
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-10-09 14:04:21 -0600

    Pass first test

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -4,5 +4,9 @@
 //
 
 export const decodedValue = (inputColors) => {
-  const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
+  const allColors = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
+  const number1 = allColors.indexOf(inputColors[0]);
+  const number2 = allColors.indexOf(inputColors[1]);
+  const number = `${number1}${number2}`
+  return parseInt(number);
 };

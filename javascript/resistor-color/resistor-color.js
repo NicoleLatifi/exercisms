fe8263e514ad0373f7554dccf4e6efeb66975773
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-10-08 20:47:34 -0600

    Move colors array outside of function to pass final test

--- javascript/resistor-color/resistor-color.js
+++ javascript/resistor-color/resistor-color.js
@@ -1,33 +1,10 @@
 //
 // This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
 // convenience to get you started writing code faster.
-
-// Black: 0
-// Brown: 1
-// Red: 2
-// Orange: 3
-// Yellow: 4
-// Green: 5
-// Blue: 6
-// Violet: 7
-// Grey: 8
-// White: 9
 //
 
 export const colorCode = (colorName) => {
-  const colors = {
-    black: 0,
-    brown: 1,
-    red: 2,
-    orange: 3,
-    yellow: 4,
-    green: 5,
-    blue: 6,
-    violet: 7,
-    grey: 8,
-    white: 9,
-  }
-  return colors[colorName]
+  return COLORS.indexOf(colorName)
 };
 
-export const COLORS = undefined;
+export const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];

122986b7751e932b14ea6887c68a44780c6002a0
Nicole Latifi <Nicole.Latifi@gmail.com>
2021-01-02 20:32:21 -0700

    Pass all but last test

--- typescript/word-count/word-count.ts
+++ typescript/word-count/word-count.ts
@@ -1,10 +1,17 @@
 class Words {
   count(words: string) {
-    const wordList = words.split(' ')
+    words = words.replace('\n', ' ')
+    words = words.replace('\t', ' ')
+    words = words.trim()
+    let wordList = words.split(' ')
     const wordCounts: {[k: string]: number} = {}
     wordList.forEach(word => {
-      wordCounts[word] ? wordCounts[word]++ : wordCounts[word] = 1
-    }) 
+      if (word !== '') {
+        word = word.toLowerCase()
+        wordCounts[word] ? wordCounts[word]++ : wordCounts[word] = 1
+      }
+    })
+    console.log(Object.entries(wordCounts))
     return new Map(Object.entries(wordCounts))
   }
 }

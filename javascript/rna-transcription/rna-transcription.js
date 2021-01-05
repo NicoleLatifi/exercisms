292a1a43fb379d3bbc11a79c4f72ac8b24344449
Nicole Latifi <Nicole.Latifi@gmail.com>
2021-01-04 21:24:25 -0700

    Remove comments

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,8 +1,3 @@
-//
-// This is only a SKELETON file for the 'RNA Transcription' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export const toRna = (dna) => {
   const rna = dna.split('').map(dnaNucleotide => {
     const nucleotideComplements = {

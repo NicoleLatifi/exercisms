91df160ca6342086ad7ad8e47047e83c37c893e0
Nicole Latifi <Nicole.Latifi@gmail.com>
2021-01-05 20:54:36 -0700

    Move nucleotideComplements object out of iterator and add trailing comma

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,11 +1,11 @@
 export const toRna = (dna) => {
+  const nucleotideComplements = {
+    'G': 'C',
+    'C': 'G',
+    'T': 'A',
+    'A': 'U',
+  }
   const rna = dna.split('').map(dnaNucleotide => {
-    const nucleotideComplements = {
-      'G': 'C',
-      'C': 'G',
-      'T': 'A',
-      'A': 'U'
-    }
     return nucleotideComplements[dnaNucleotide]
   })
   return rna.join('')

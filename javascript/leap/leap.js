d89c8adef3d19a932db49cd6d404bf794acfe266
Nicole Latifi <Nicole.Latifi@gmail.com>
2021-01-05 21:24:30 -0700

    Determine if leap year

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,26 +1,15 @@
-//
-// This is only a SKELETON file for the 'Leap' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export const isLeap = (year) => {
-  let divisibleBy4
-  let divisibleBy100
-  let divisibleBy400
-  let leapDetermination
-  // if (year %= 4 === 0) {
-  //   return true
-  // } else {
-  //   return false
-  // }
-  year % 4 === 0 ? divisibleBy4 = true : divisibleBy4 = false
-  year % 100 === 0 ? divisibleBy100 = true : divisibleBy100 = false
-  year % 400 === 0 ? divisibleBy400 = true : divisibleBy400 = false
-  console.log(divisibleBy4)
-  if (divisibleBy4 ) {
+  let check = {
+    divisibleBy4: year % 4 === 0 ? true : false,
+    divisibleBy100: year % 100 === 0 ? true : false,
+    divisibleBy400: year % 400 === 0 ? true : false,
+  }
+  let leapDetermination = false
+  if (
+    check.divisibleBy4 && !check.divisibleBy100 ||
+    check.divisibleBy4 && check.divisibleBy100 && check.divisibleBy400
+    ) {
     leapDetermination = true
-  } else {
-    leapDetermination = false
   }
   return leapDetermination
 };

12f50ba5837490b580f8d5d7b0cebae0c4b8b8d6
Nicole Latifi <Nicole.Latifi@gmail.com>
2021-01-05 21:33:04 -0700

    Refactor using single ternary operator

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,15 +1,4 @@
 export const isLeap = (year) => {
-  let check = {
-    divisibleBy4: year % 4 === 0 ? true : false,
-    divisibleBy100: year % 100 === 0 ? true : false,
-    divisibleBy400: year % 400 === 0 ? true : false,
-  }
-  let leapDetermination = false
-  if (
-    check.divisibleBy4 && !check.divisibleBy100 ||
-    check.divisibleBy4 && check.divisibleBy100 && check.divisibleBy400
-    ) {
-    leapDetermination = true
-  }
+  let leapDetermination = year % 100 === 0 ? year % 400 === 0 : year % 4 === 0;
   return leapDetermination
 };

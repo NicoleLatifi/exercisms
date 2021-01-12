4e8e84258b524c26407ebb9cb48b209e1360d814
Nicole Latifi <Nicole.Latifi@gmail.com>
2021-01-11 21:44:49 -0700

    Write function to pass all tests

---
+++ javascript/space-age/space-age.js
@@ -0,0 +1,20 @@
+const earthYearsByPlanet = {
+  "mercury": 0.2408467, 
+  "venus": 0.61519726, 
+  "earth": 1.0,
+  "mars": 1.8808158, 
+  "jupiter": 11.862615, 
+  "saturn": 29.447498, 
+  "uranus": 84.016846, 
+  "neptune": 164.79132,
+}
+
+const secondsToEarthYears = (seconds) => {
+  const earthYears = seconds / 31557600
+  return earthYears
+}
+
+export const age = (planet, seconds) => {
+  const age = secondsToEarthYears(seconds) / earthYearsByPlanet[planet]
+  return +age.toFixed(2)
+};

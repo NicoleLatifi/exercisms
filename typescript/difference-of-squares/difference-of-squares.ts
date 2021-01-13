e4efe3564fe5ca91815423f6025c7b5fef3fb7d7
Nicole Latifi <Nicole.Latifi@gmail.com>
2021-01-12 21:01:43 -0700

    Create sumOfSquares and difference

--- typescript/difference-of-squares/difference-of-squares.ts
+++ typescript/difference-of-squares/difference-of-squares.ts
@@ -1,26 +1,29 @@
 export class Squares {
   num: number;
   squareOfSum: number;
+  sumOfSquares: number;
+  difference: number;
 
   constructor(n: number) {
     this.num = n;
     this.squareOfSum = this.calcSquareOfSum();
-
+    this.sumOfSquares = this.calcSumOfSquares();
+    this.difference = this.squareOfSum - this.sumOfSquares
   }
 
   calcSquareOfSum(): number {
     let result: number = 0;
     for (let i = this.num; i > 0; i--) {
-      result = result + i
+      result += i
     }
-    return (result ** 2)
+    return result ** 2
   }
   
-  sumOfSquares() {
-
-  }
-
-  difference() {
-
+  calcSumOfSquares(): number {
+    let result: number = 0;
+    for (let i = this.num; i > 0; i--) {
+      result += (i ** 2)
+    }
+    return result
   }
 }

53f7e0dd1df693dbd17918fc36e96127c6e26966
Nicole Latifi <Nicole.Latifi@gmail.com>
2021-01-24 20:43:11 -0700

    Check each letter, when not found return false

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -1,5 +1,16 @@
+// declare a variable pangram = true
+// iterate over the unicodes for a-z (097 - 122)
+// while pangram = true,for every unicode, check to see if it occurs in the input sentence
+//     if it doesn't, pangram = false
+// return pangram
+
 export const isPangram = (sentence) => {
-  for(let i = 0; i < sentence.length; i++) {
-    console.log(sentence.charCodeAt(i))
+  sentence = sentence.toLowerCase()
+  let pangram = true;
+  let i = 97;
+  while (i <= 122 && pangram === true) {
+    pangram = sentence.includes(String.fromCharCode(i))
+    i++
   }
+  return pangram
 };
